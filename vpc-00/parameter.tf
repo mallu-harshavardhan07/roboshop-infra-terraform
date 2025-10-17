1f58927c0ed53ba7c22bd1154ee29bdbfc196af7
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-17 09:26:57 +0530

    done

--- vpc-00/parameter.tf
+++ vpc-00/parameter.tf
@@ -1,5 +1,11 @@
-resource "aws_ssm_parameter" "vpc_id" {
+resource "aws_ssm_parameter" "store_vpc_id" {
   name  = "practice/prod/vpc_id"
   type  = "String"
   value = module.create_vpc.vpc_id
 }
+
+
+# This is for getting data from the aws_ssm_parameter
+# data "aws_ssm_parameter" "get_vpc_id" {
+#   name = "/practice/prod/vpc_id"
+# }

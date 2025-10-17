b82a2705fdbc1d5f64fb18de5dbddb1c16617dd0
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-17 11:17:53 +0530

    done

--- sg-10/main.tf
+++ sg-10/main.tf
@@ -2,9 +2,9 @@ module "create_sg" {
     source = "git::https://github.com/mallu-harshavardhan07/security_group-module.git?ref=main"
     project = var.project
     env = var.env
-    sg_name = "${var.project}-frontend"
+    sg_name = "frontend"
     sg_description = var.sg_description
-    vpc_id = data.aws-aws_ssm_parameter.get_vpc_id
+    vpc_id = data.aws_ssm_parameter.get_vpc_id
 
 
 }

e9956fef9a8e687959b0982f8ed176ea4f981646
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-17 11:21:20 +0530

    done

--- sg-10/data.tf
+++ sg-10/data.tf
@@ -1,3 +1,3 @@
 data "aws_ssm_parameter" "get_vpc_id" {
-  name = "practice/prod/vpc_id"
+  name = "/practice/prod/vpc_id"
 }
--- sg-10/parameters.tf
+++ sg-10/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "store_Sg_id" {
+resource "aws_ssm_parameter" "store_sg_id" {
   name  = "/${var.project}/${var.env}/sg_id"
   type  = "String"
   value = module.create_sg.sg_id

563a4016493f7c4ff6cf3c030786dbc5b97bb476
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-17 08:59:04 +0530

    done

--- vpc-00/main.tf
+++ vpc-00/main.tf
@@ -5,7 +5,7 @@
     vpc_cidr_block = "10.0.0.0/16"
     public_cidr_subnet = ["10.0.1.0/24","10.0.2.0/24"]
     private_cidr_subnet = ["10.0.11.0/24","10.0.12.0/24"]
-    database_cidr_ = ["10.0.21.0/24","10.0.22.0/24"]
+    database_cidr_subnet = ["10.0.21.0/24","10.0.22.0/24"]
     is_peering_required = false
 
   

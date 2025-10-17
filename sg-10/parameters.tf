275d6eccca8e281d077eb148c44d9365b2f4207e
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-17 11:19:40 +0530

    done

--- sg-10/parameters.tf
+++ sg-10/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "store_Sg_id" {
-  name  = "${var.project}/${var.env}/sg_id"
+  name  = "/${var.project}/${var.env}/sg_id"
   type  = "String"
   value = module.create_sg.sg_id
 }
